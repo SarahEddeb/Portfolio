bf89a7b3fd479fb4ea9cb33ec8970785fad4e099
SarahEddeb <sarah.ed123@gmail.com>
2024-07-24 11:14:10 -0400

    Add env configurations

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { basePath: "/Portfolio", output: "export" };
+const nextConfig = {
+  basePath: process.env.NODE_ENV === "production" ? "/Portfolio" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/Portfolio/" : "",
+  output: "export",
+};
 
 export default nextConfig;
